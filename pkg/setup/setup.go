5b261400f7911e2b7d265b22a27313a121c7eb88
ikafly144 <support@sabafly.net>
2022-11-02 21:32:39 +0900

    ロールパネルを変更できるようにした

--- pkg/setup/setup.go
+++ pkg/setup/setup.go
@@ -215,6 +215,10 @@ func Setup() (*discordgo.Session, []*discordgo.ApplicationCommand, bool, string)
 				},
 			},
 		},
+		{
+			Name: "modify",
+			Type: discordgo.MessageApplicationCommand,
+		},
 	}
 	var (
 		commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
@@ -316,7 +320,7 @@ func Setup() (*discordgo.Session, []*discordgo.ApplicationCommand, bool, string)
 								discordgo.ActionsRow{
 									Components: []discordgo.MessageComponent{
 										discordgo.SelectMenu{
-											CustomID:  "role_panel",
+											CustomID:  "gobot_panel_role",
 											MinValues: &zero,
 											Options: []discordgo.SelectMenuOption{
 												{
@@ -331,26 +335,101 @@ func Setup() (*discordgo.Session, []*discordgo.ApplicationCommand, bool, string)
 						}
 						_, err := s.ChannelMessageSendComplex(cid, &content)
 						if err != nil {
-							s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-								Type: discordgo.InteractionResponseChannelMessageWithSource,
-								Data: &discordgo.InteractionResponseData{
-									Content: fmt.Sprint(err),
-									Flags:   discordgo.MessageFlagsEphemeral,
-								},
+							str := fmt.Sprint(err)
+							s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+								Content: &str,
+							})
+						} else {
+							str := "ロールを追加するにはメッセージを右クリックまたは長押しして「アプリ」から「編集」を押してください"
+							s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+								Content: &str,
 							})
 						}
-						str := "ロールを追加するにはメッセージを右クリックまたは長押しして「アプリ」から「編集」を押してください"
-						s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
-							Content: &str,
-						})
 					}
 				}
+			},
+			"modify": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+				data := &discordgo.ApplicationCommandInteractionData{}
+				byte, _ := json.Marshal(i.Interaction.Data)
+				json.Unmarshal(byte, data)
+				mes, err := s.ChannelMessage(i.ChannelID, data.TargetID)
+				if err != nil {
+					log.Print(err)
+				}
+				if len(mes.Components) != 0 {
+					for _, v := range mes.Components {
+						if v.Type() == discordgo.ActionsRowComponent {
+							byte, _ := v.MarshalJSON()
+							data := &discordgo.ActionsRow{}
+							json.Unmarshal(byte, data)
+							for _, v := range data.Components {
+								if v.Type() == discordgo.SelectMenuComponent {
+									byte, _ := v.MarshalJSON()
+									data := &discordgo.SelectMenu{}
+									json.Unmarshal(byte, data)
+									if data.CustomID == "gobot_panel_role" {
+										roles, _ := s.GuildRoles(i.GuildID)
+										options := []discordgo.SelectMenuOption{}
+										me, _ := s.GuildMember(i.GuildID, *ApplicationId)
+										var highestPosition int
+										for _, v := range me.Roles {
+											r, _ := s.State.Role(i.GuildID, v)
+											if r.Position > highestPosition {
+												highestPosition = r.Position
+											}
+										}
+										for _, v := range roles {
+											if v.Position < highestPosition && !v.Managed && v.ID != i.GuildID {
+												options = append(options, discordgo.SelectMenuOption{
+													Label: v.Name,
+													Value: v.ID,
+												})
+											}
+										}
+										zero := 0
+										s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+											Type: discordgo.InteractionResponseChannelMessageWithSource,
+											Data: &discordgo.InteractionResponseData{
+												Content: "追加するロールを選んでください",
+												Embeds: []*discordgo.MessageEmbed{
+													{
+														Title: mes.ID,
+													},
+												},
+												Flags: discordgo.MessageFlagsEphemeral,
+												Components: []discordgo.MessageComponent{
+													discordgo.ActionsRow{
+														Components: []discordgo.MessageComponent{
+															discordgo.SelectMenu{
+																CustomID:  "gobot_panel_role_add",
+																MinValues: &zero,
+																MaxValues: len(options),
+																Options:   options,
+															},
+														},
+													},
+												},
+											},
+										})
+									}
+								}
+							}
+						}
+					}
+				}
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "そのメッセージには使用できません",
+						Flags:   discordgo.MessageFlagsEphemeral,
+					},
+				})
 			},
 		}
 	)
 
 	messageComponentHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"role_panel": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		"gobot_panel_role": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			component := i.Message.Components
 			var content string
 			bytes, _ := component[0].MarshalJSON()
@@ -381,6 +460,65 @@ func Setup() (*discordgo.Session, []*discordgo.ApplicationCommand, bool, string)
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
 					Content: content,
+					Flags:   discordgo.MessageFlagsEphemeral,
+				},
+			})
+		},
+		"gobot_panel_role_add": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			mid := i.Message.Embeds[0].Title
+			gid := i.GuildID
+			cid := i.ChannelID
+			mes, _ := s.ChannelMessage(cid, mid)
+			rv := i.MessageComponentData().Values
+			roles := []discordgo.Role{}
+			for _, v := range rv {
+				role, _ := s.State.Role(gid, v)
+				roles = append(roles, *role)
+			}
+			options := []discordgo.SelectMenuOption{}
+			for _, r := range roles {
+				options = append(options, discordgo.SelectMenuOption{
+					Label: r.Name,
+					Value: r.ID,
+				})
+			}
+			var fields string
+			for _, r := range roles {
+				fields += r.Mention() + "\r"
+			}
+			zero := 0
+			content := discordgo.MessageEdit{
+				ID:      mid,
+				Channel: cid,
+				Embeds: []*discordgo.MessageEmbed{
+					{
+						Title: mes.Embeds[0].Title,
+						Fields: []*discordgo.MessageEmbedField{
+							{
+								Name:  "roles",
+								Value: fields,
+							},
+						},
+					},
+				},
+				Components: []discordgo.MessageComponent{
+					discordgo.ActionsRow{
+						Components: []discordgo.MessageComponent{
+							discordgo.SelectMenu{
+								CustomID:  "gobot_panel_role",
+								MinValues: &zero,
+								MaxValues: len(options),
+								Options:   options,
+							},
+						},
+					},
+				},
+			}
+			s.ChannelMessageEditComplex(&content)
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "OK",
 					Flags:   discordgo.MessageFlagsEphemeral,
 				},
 			})
