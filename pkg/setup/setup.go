b8da84184d0fe80f08f1b57805cff3329bd82b63
ikafly144 <support@sabafly.net>
2022-11-06 14:38:22 +0900

    関数化

--- pkg/setup/setup.go
+++ pkg/setup/setup.go
@@ -1,7 +1,6 @@
 package setup
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -347,83 +346,7 @@ func Setup() (*discordgo.Session, []*discordgo.ApplicationCommand, bool, string)
 
 	modalSubmitHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate, mid string){
 		"gobot_panel_minecraft_add_modal": func(s *discordgo.Session, i *discordgo.InteractionCreate, mid string) {
-			var name string
-			var address string
-			var port int
-			for _, mc := range i.ModalSubmitData().Components {
-				if mc.Type() == discordgo.ActionsRowComponent {
-					bytes, _ := mc.MarshalJSON()
-					data := &discordgo.ActionsRow{}
-					json.Unmarshal(bytes, data)
-					bytes, _ = data.Components[0].MarshalJSON()
-					text := &discordgo.TextInput{}
-					json.Unmarshal(bytes, text)
-					switch text.CustomID {
-					case "gobot_panel_minecraft_add_servername":
-						name = text.Value
-					case "gobot_panel_minecraft_add_address":
-						address = text.Value
-					case "gobot_panel_minecraft_add_port":
-						i, _ := strconv.Atoi(text.Value)
-						port = i
-					}
-				}
-			}
-			mes, _ := s.ChannelMessage(i.ChannelID, mid)
-			bytes, _ := mes.Components[0].MarshalJSON()
-			data := &discordgo.ActionsRow{}
-			json.Unmarshal(bytes, data)
-			bytes, _ = data.Components[0].MarshalJSON()
-			text := &discordgo.SelectMenu{}
-			json.Unmarshal(bytes, text)
-			options := text.Options
-			str := strings.Split(options[0].Value, ":")
-			bl, _ := strconv.ParseBool(str[3])
-			options = append(options, discordgo.SelectMenuOption{
-				Label: name,
-				Value: name + ":" + address + ":" + strconv.Itoa(port) + ":" + strconv.FormatBool(bl),
-			})
-			name = strings.ReplaceAll(name, ":", ";")
-			address = strings.ReplaceAll(address, ":", ";")
-			if port > 65535 || 1 > port {
-				port = 25565
-			}
-			zero := 0
-			res := discordgo.MessageEdit{
-				Channel: i.ChannelID,
-				ID:      mid,
-				Embeds:  mes.Embeds,
-				Components: []discordgo.MessageComponent{
-					discordgo.ActionsRow{
-						Components: []discordgo.MessageComponent{
-							discordgo.SelectMenu{
-								CustomID:  "gobot_panel_minecraft",
-								Options:   options,
-								MinValues: &zero,
-								MaxValues: 1,
-							},
-						},
-					},
-				},
-			}
-			_, err := s.ChannelMessageEditComplex(&res)
-			if err != nil {
-				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: fmt.Sprint(err),
-						Flags:   discordgo.MessageFlagsEphemeral,
-					},
-				})
-			}
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: "OK",
-					Flags:   discordgo.MessageFlagsEphemeral,
-				},
-			})
-			log.Print(name + address + strconv.Itoa(port))
+			command.MSminecraftPanel(s, i, mid)
 		},
 	}
 
