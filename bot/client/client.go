d6205a1950f32609319a9267ac3cfe77a47ed6da
ikafly144 <support@sabafly.net>
2023-06-09 00:05:45 +0900

    add: message log (#113)

--- bot/client/client.go
+++ bot/client/client.go
@@ -3,17 +3,26 @@ package client
 import (
 	"github.com/disgoorg/snowflake/v2"
 	"github.com/sabafly/gobot/bot/db"
+	"github.com/sabafly/sabafly-lib/v2/logging"
 )
 
 func New(cfg *Config, db db.DB) (*Client, error) {
+	ml, err := logging.New(logging.Config{
+		LogPath: "./logs/messages",
+	})
+	if err != nil {
+		return nil, err
+	}
 	return &Client{
-		Config: cfg,
-		DB:     db,
+		Config:        cfg,
+		DB:            db,
+		MessageLogger: ml,
 	}, nil
 }
 
 type Client struct {
-	Config     *Config
-	DB         db.DB
-	MessagePin map[snowflake.ID]db.GuildMessagePins
+	Config        *Config
+	DB            db.DB
+	MessagePin    map[snowflake.ID]db.GuildMessagePins
+	MessageLogger *logging.Logging
 }
