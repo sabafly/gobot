b8bbdae7fe8e44a851c8b546c8f43e60254feba7
ikafly144 <support@sabafly.net>
2023-12-05 23:59:00 +0900

    テストを加える
    Signed-off-by: ikafly144 <support@sabafly.net>

--- internal/xppoint/xp_test.go
+++ internal/xppoint/xp_test.go
@@ -9,10 +9,16 @@ import (
 
 func TestXPSum(t *testing.T) {
 	for i := 0; i < 1000; i++ {
-		pt := xppoint.TotalPoint(int64(i))
-		xp := xppoint.XP(pt)
+		tp := xppoint.TotalPoint(int64(i))
+		xp := xppoint.XP(tp)
 		if xp.Level() != int64(i) {
 			slog.Warn("failed 1", "xp", xp, "i", i, "level", xp.Level())
+			t.Fail()
+		}
+		rp := xppoint.RequiredPoint(int64(i))
+		xp.Add(rp)
+		if xp.Level() != int64(i)+1 {
+			slog.Warn("failed 2", "xp", xp, "i", i, "level", xp.Level())
 			t.Fail()
 		}
 	}
