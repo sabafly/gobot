15252ef40d25fbe8d427723ef01d6694e67a869a
ikafly144 <support@sabafly.net>
2024-10-21 12:21:26 +0900

    Add bulk delete message command
    Fixes #44
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/sabafly/gobot/issues/44?shareId=XXXX-XXXX-XXXX-XXXX).

--- bot/commands/message/message.go
+++ bot/commands/message/message.go
@@ -215,6 +215,19 @@ func Command(c *components.Components) *generic.Command {
 							},
 						},
 					},
+					discord.ApplicationCommandOptionSubCommand{
+						Name:        "bulk_delete",
+						Description: "bulk delete messages",
+						Options: []discord.ApplicationCommandOption{
+							discord.ApplicationCommandOptionInt{
+								Name:        "count",
+								Description: "number of messages to delete",
+								Required:    true,
+								MinValue:    2,
+								MaxValue:    100,
+							},
+						},
+					},
 				},
 			},
 		},
@@ -553,6 +566,39 @@ func Command(c *components.Components) *generic.Command {
 					); err != nil {
 						return errors.NewError(err)
 					}
+					return nil
+				},
+			},
+			"/message/bulk_delete": generic.PCommandHandler{
+				Permission: []generic.Permission{
+					generic.PermissionString("message.bulk_delete"),
+				},
+				DiscordPerm: discord.PermissionManageMessages,
+				CommandHandler: func(c *components.Components, event *events.ApplicationCommandInteractionCreate) errors.Error {
+					count := event.SlashCommandInteractionData().Int("count")
+					messages, err := event.Client().Rest().GetMessages(event.Channel().ID(), count)
+					if err != nil {
+						return errors.NewError(err)
+					}
+
+					messageIDs := make([]snowflake.ID, len(messages))
+					for i, message := range messages {
+						messageIDs[i] = message.ID
+					}
+
+					if err := event.Client().Rest().BulkDeleteMessages(event.Channel().ID, messageIDs); err != nil {
+						return errors.NewError(err)
+					}
+
+					if err := event.CreateMessage(
+						discord.NewMessageBuilder().
+							SetContent(translate.Message(event.Locale(), "components.message.bulk_delete.success", translate.WithTemplate(map[string]any{"Count": count}))).
+							SetFlags(discord.MessageFlagEphemeral).
+							BuildCreate(),
+					); err != nil {
+						return errors.NewError(err)
+					}
+
 					return nil
 				},
 			},
